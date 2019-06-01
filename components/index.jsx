8a20fa144228c3478da22477bfaedf4a6846c12c
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-01 16:16:11 -0500

    initial testing

--- components/index.jsx
+++ components/index.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import Autosuggest from 'react-autosuggest';
 import ButtonAppBar from './appbar.jsx';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+
 
 ReactDOM.render(<ButtonAppBar/>, document.getElementById('searchbar'))
 
