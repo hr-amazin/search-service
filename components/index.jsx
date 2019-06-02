364ace51209ec43d3e767921a5fa485407024081
Jimmy Hung <hungmeenhong@gmail.com>
2019-06-02 15:37:52 -0500

    v1

--- components/index.jsx
+++ components/index.jsx
@@ -157,7 +157,7 @@ class Search extends React.Component {
     
     submit(e){
       e.preventDefault();
-      // this.props.setUuid(parseInt(this.state.value)||1001);
+      this.props.setUuid(parseInt(this.state.value)||1001);
     }
   
     render(){
